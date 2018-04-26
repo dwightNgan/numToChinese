f87643e84b8b193b45de3581b269085f4525a8e8
dwightNgan <327315472@qq.com>
2018-04-26 23:53:39 +0800

    功能：可以处理万亿级数字(17位数)

--- src/index.js
+++ src/index.js
@@ -1,35 +1,41 @@
 export default class numToChinese {
   constructor(){
     this.char = ['〇','一','二','三','四','五','六','七','八','九','十'];
-    this.units = ['','十','百','千','万'];
+    this.baseUnits = ['','十','百','千'];
+    this.quaUnits = ['','万','亿','万亿'];
   }
-  parse (number) {
-    const { char, units } = this;
-    const numArr = number.toString().split('');
+  parseInt (number) {
+    const { char, baseUnits, quaUnits } = this;
+    const numArr = number.toString().split('').reverse();
     const { length } = numArr;
-    const maxIndex = length - 1;
-    // let getUnit = (index) => {
-    //   let qua = parseInt(index / 4);
-    //   let remainder = index % 4;
-    //   let startIndex = 0;
-    //   let unit = ''; 
-    //   let loop = (idx) => {
-    //     unit = units[idx] + unit;
-    //     if(startIndex === qua) return;
-    //     loop()
-    //     startIndex++;
-
-    //   }
-
-    // }
-    let charArr = numArr.map((num, index) => {
-      if(length === 2 && num === "1" && index === 0){
-        return units[maxIndex - index]
-      }
-      return num === "0" ? "零" : char[num] + units[maxIndex - index];
+    
+    let sliceArr = [];
+    numArr.forEach((num, index) => {
+      const quotient = parseInt(index / 4);
+      if(!sliceArr[quotient]) {
+        sliceArr[quotient] = []
+      };
+      sliceArr[quotient].push(num)
     });
-    return charArr.join('').replace(/零*$/,'').replace(/零+/, "零");
+
+    let genCombination = (num, index) => {
+      const quotient = parseInt(index /4);
+      let combine = num === "0" ? '零' : char[num] + baseUnits[index % 4 ];
+      if(index % 4 === 0 && index !== 0) {
+        combine += quaUnits[quotient - 1];
+      }
+      return combine
+    }
+
+    let result = sliceArr.map((group, index) => {
+      let groupStr = group.map(genCombination).reverse().join('');
+      groupStr = groupStr.replace(/零+/g,'零').replace(/零+$/,'');
+      return groupStr ? groupStr + quaUnits[index] : '';
+    }).reverse().join('');
+
+    return result
   }
+
 }
 
-console.log(new numToChinese().parse(101));
+console.log(new numToChinese().parseInt(1000));
